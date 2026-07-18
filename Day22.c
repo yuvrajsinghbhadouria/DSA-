48b87f0456d190333572c63b14e605ec75e7f175
yuvrajsinghbhadouria <yuvrajsinghbhadouria19@outlook.com>
2026-07-18 21:38:30 +0530

    day22 DSA Problem

---
+++ Day22.c
@@ -0,0 +1,67 @@
+/*Problem: Count Nodes in Linked List
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the result
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+// Create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Count nodes in the linked list
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        struct Node* newNode = createNode(x);
+
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    printf("%d\n", countNodes(head));
+
+    return 0;
+}
